017ebd99ade96685b936ac7b6b5bdb2e1ae1c957
Leesungsup <tmdtjq0116@naver.com>
2021-06-18 00:32:10 +0900

    admin

--- crawler.py
+++ crawler.py
@@ -4,8 +4,9 @@ import django
 django.setup()
 from quiz.models import Player_info
 import requests
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-import pandas as pd
+#import pandas as pd
 def crawler():
     headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x32) AppleWebKit/536.35 (KHTML, like Gecko) Chrome/100.0.0.7 Safari/536.28"}
     url="https://www.transfermarkt.com/spieler-statistik/wertvollstespieler/marktwertetop"
@@ -15,7 +16,6 @@ def crawler():
         soup=BeautifulSoup(response.content,'html.parser')
         string=soup.find_all('tr',{'class':['odd','even']})
     for info in string:
-        number=imformation[0].get_text()
         information=info.find_all('td')
         number=information[0].get_text()
         name=information[3].get_text()
@@ -24,10 +24,12 @@ def crawler():
         nation=information[6].img['alt']
         team=information[7].img['alt']
         value=information[8].span['title']
+        print(name)
         player_list.append([number,name,position,age,nation,team,value])
     #df=pd.DataFrame(player_list,columns=['number','name','position','age','nation','team','value'])
     return player_list
 if __name__=='__main__':
     data = crawler()
+
     for item in data:
         Player_info(number = item[0], name = item[1], position = item[2],age=item[3],nation=item[4],team=item[5],value=item[6]).save()
