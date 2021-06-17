04ad33be8966b4ef22a17b89ab3da2f46dd5ab7f
Leesungsup <tmdtjq0116@naver.com>
2021-06-18 00:37:56 +0900

    admin

--- crawler.py
+++ crawler.py
@@ -11,12 +11,14 @@ def crawler():
     headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x32) AppleWebKit/536.35 (KHTML, like Gecko) Chrome/100.0.0.7 Safari/536.28"}
     url="https://www.transfermarkt.com/spieler-statistik/wertvollstespieler/marktwertetop"
     response=requests.get(url,headers=headers)
-    player_list=[]
     if response.status_code==200:
         soup=BeautifulSoup(response.content,'html.parser')
         string=soup.find_all('tr',{'class':['odd','even']})
     for info in string:
         information=info.find_all('td')
+        player_list={'number':number,
+        'name':name,'position':position,
+        'age':age,'nation':nation,'team':team,'value':value}
         number=information[0].get_text()
         name=information[3].get_text()
         position=information[4].get_text()
@@ -24,8 +26,9 @@ def crawler():
         nation=information[6].img['alt']
         team=information[7].img['alt']
         value=information[8].span['title']
-        print(name)
-        player_list.append([number,name,position,age,nation,team,value])
+        player_list={'number':number,
+        'name':name,'position':position,
+        'age':age,'nation':nation,'team':team,'value':value}
     #df=pd.DataFrame(player_list,columns=['number','name','position','age','nation','team','value'])
     return player_list
 if __name__=='__main__':
