43a1d235daa06f8a838e3bdde6a6f8c7ae290209
Leesungsup <tmdtjq0116@naver.com>
2021-06-18 00:07:17 +0900

    admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Quiz
+from .models import Player_info
 # Register your models here.
 admin.site.register(Quiz)
+admin.site.register(Player_info)
