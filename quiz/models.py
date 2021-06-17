9f0f5c699d7a73661d424783f3a1250355c0b4db
Leesungsup <tmdtjq0116@naver.com>
2021-06-17 23:59:19 +0900

    models

--- quiz/models.py
+++ quiz/models.py
@@ -10,7 +10,7 @@ class Player_info(models.Model):
     number = models.IntegerField(primary_key = True)
     name = models.CharField(max_length=50)
     position = models.CharField(max_length=50)
-    age=models.InterField()
+    age=models.IntegerField()
     nation=models.CharField(max_length=40)
     team=models.CharField(max_length=40)
     value=models.CharField(max_length=20)
