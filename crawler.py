01b728cf28590af2936fe5e6edca743ade9ed4e2
Leesungsup <tmdtjq0116@naver.com>
2021-06-18 14:32:08 +0900

    'ko'

--- crawler.py
+++ crawler.py
@@ -6,20 +6,24 @@ from quiz.models import Player_info
 import requests
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
-#import pandas as pd
+import pandas as pd
 def crawler():
     headers={'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x32) AppleWebKit/536.35 (KHTML, like Gecko) Chrome/100.0.0.7 Safari/536.28"}
     url=f"https://www.transfermarkt.com/spieler-statistik/wertvollstespieler/marktwertetop?ajax=yw1&page=1"
     response=requests.get(url,headers=headers)
     player_list=[]
+    number=[]
+    name=[]
+    position=[]
+    age=[]
+    nation=[]
+    team=[]
+    value=[]
     if response.status_code==200:
         soup=BeautifulSoup(response.content,'html.parser')
         string=soup.find_all('tr',{'class':['odd','even']})
     for info in string:
         information=info.find_all('td')
-        player={'number':number,
-        'name':name,'position':position,
-        'age':age,'nation':nation,'team':team,'value':value}
         number=information[0].get_text()
         name=information[3].get_text()
         position=information[4].get_text()
