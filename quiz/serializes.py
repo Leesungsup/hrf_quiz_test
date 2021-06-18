a45084c9831ede53ddf665a5eb276547918506c0
Leesungsup <tmdtjq0116@naver.com>
2021-06-18 13:42:46 +0900

    serial

--- quiz/serializes.py
+++ quiz/serializes.py
@@ -1,6 +1,10 @@
 from rest_framework import serializers
-from .models import Quiz
+from .models import Quiz,Player_info
 class QuizSerializer(serializers.ModelSerializer):
     class Meta:
         model=Quiz
         fields=('title','body','answer')
+class PlayerSerializer(serializers.ModelSerialize):
+    class Meta:
+        model=Player_info
+        fields=('number','name','position','age','nation','team','value')
